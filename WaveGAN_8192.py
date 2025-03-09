3b235c4dc85cadeac40e1e9e3d97b0faecb0a481
RafKal <114592563+RafKal@users.noreply.github.com>
2025-03-09 11:58:37 +0100

    Add files via upload

---
+++ WaveGAN_8192.py
@@ -0,0 +1,344 @@
+import torch.nn as nn
+from config.config_bases import ModelConfigBase
+import torch.optim as optim
+import torch
+import models.utils.pt_training_loops as training_loops
+from torch.utils.data import DataLoader
+from models.utils.pt_utils import repeat_vector
+from models.utils.pt_utils import TimeDistributed
+from torchview import draw_graph
+import pandas as pd
+from models.utils.WaveGAN_utils import gradient_penalty, init_weights, get_modules
+import torch.nn.functional as F
+
+
+
+class GanConfig(ModelConfigBase):
+    def __init__(self):
+        self.lr = 1e-4 
+        self.sequence_length = 8192
+        self.n_channels = 0  
+        self.z_dim = 100   
+        self.batch_size = 64 
+        self.model_size = 64
+        self.shift_factor = 2 # Phase Shuffle
+        
+        
+        # For W-GAN-GP
+        self.critic_iterations = 5  
+        self.lambda_GP = 10 
+
+        
+
+
+class Gan(nn.Module):
+    def __init__(self, model_config: GanConfig, device):
+        super().__init__()
+        self.model_config = model_config
+        self.device = device
+        self.tracker = GanLossTracker()
+        
+        
+        self.critic = Critic(model_config.n_channels, model_config.batch_size,
+                                         model_config.model_size, model_config.shift_factor).to(device)
+        init_weights(self.critic)
+        
+        
+        self.gen = Generator(model_config.z_dim, model_config.n_channels,
+                              model_config.batch_size , model_config.model_size ).to(device)
+        init_weights(self.gen)
+        
+        gen_params, critic_params = get_modules(self)
+        self.opt_gen = optim.Adam(gen_params,  betas=[0.5, 0.9])
+        self.opt_critic = optim.Adam(critic_params, betas=[0.5, 0.9])
+         
+       
+        self.data_loss = torch.nn.MSELoss()
+
+    def forward(self, x):
+
+        noise = 2 * (torch.rand(size=(self.model_config.batch_size, self.model_config.z_dim)).to(self.device)) -1
+        fake = self.gen(noise)
+
+        return fake      
+
+
+
+    def test_step(self, real):
+        return
+
+    def train_step(self, real):
+        
+        # PyTorch needs format of [batch size, sequence length, n channels]
+        real = torch.permute(real, (0, 2, 1))
+        
+
+        # Train Critic: max E[critic(real)] - E[critic(fake)]
+        # equivalent to minimizing the negative of that
+        for _ in range(self.model_config.critic_iterations):
+            fake =  self.forward(1) # 1 is dummy input to forego error..
+            critic_real = self.critic(real).reshape(-1)
+            critic_fake = self.critic(fake).reshape(-1)
+            gp = gradient_penalty(self.critic, real, fake, device=self.device)
+            loss_critic = (
+                -(torch.mean(critic_real) - torch.mean(critic_fake)) + self.model_config.lambda_GP * gp
+            )
+
+            self.critic.zero_grad()
+            loss_critic.backward(retain_graph=True)
+            self.opt_critic.step()
+
+        # Train Generator: max E[critic(gen_fake)] <-> min -E[critic(gen_fake)]
+        gen_fake = self.critic(fake).reshape(-1)
+        loss_gen = -torch.mean(gen_fake)
+        self.gen.zero_grad()
+        loss_gen.backward()
+        self.opt_gen.step()
+
+        mse_loss = self.data_loss(fake, real)
+        self.tracker.add_to_train_loss(loss_critic, mse_loss)  
+
+  
+    def synthesize_data(self):
+
+        # Reshaping output to be plotted/exported
+        data = torch.permute(self.forward(1).cpu().detach(), (0, 2, 1))
+        return data
+
+
+
+    def fit(
+        self,
+        train_loader: DataLoader,
+        test_loader: DataLoader,
+        n_epochs: int,
+        gpu_device: str,
+        callbacks,
+    ):
+        return training_loops.fit(
+            self, train_loader, test_loader, n_epochs, gpu_device, callbacks
+        )
+
+
+class Generator(nn.Module):
+   def __init__(self, z_dim, data_dim, batch_size, model_size, print):
+        super(Generator, self).__init__()
+        self.net = nn.Sequential(
+   
+            nn.Linear(z_dim, 256 * model_size), 
+            Reshape([batch_size,  model_size * 8, -1]), 
+            nn.ReLU(),
+
+            self._block( model_size*8,  model_size*4, 25, 4, 11),  
+            self._block( model_size * 4, model_size * 2, 25, 4, 11),  
+            self._block(model_size * 2, model_size, 25, 4, 11),  
+            nn.ConvTranspose1d(
+                model_size, data_dim, 25, 4, 11,
+                bias=False, output_padding=1
+            ), 
+            nn.Tanh(),
+        )
+
+   def forward(self, x):
+        return self.net(x)
+  
+
+   def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose1d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+                output_padding=1
+            ),
+              nn.ReLU()
+        )
+    
+class Critic(nn.Module):
+   def __init__(self, data_dim, batch_size, model_size, print, shift_factor):
+        super(Critic, self).__init__()
+        self.disc = nn.Sequential(
+            
+            self._block(data_dim, model_size , 25, 4, 11),
+            PhaseShuffle(shift_factor), 
+            
+            self._block( model_size, model_size*2, 25, 4, 11),  
+            PhaseShuffle(shift_factor),  
+         
+            self._block( model_size * 2, model_size * 4, 25, 4, 11), 
+            PhaseShuffle(shift_factor),        
+
+            self._block(model_size * 4, model_size *8, 25, 4, 11), 
+          
+            Reshape([batch_size, 256*model_size]),
+            nn.Linear(256*model_size, 1),
+
+        )
+
+   def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv1d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.LeakyReLU(0.2),
+        )
+
+   def forward(self, x):
+        return self.disc(x)
+
+
+class GanLossTracker:
+    def __init__(self):
+        self.init_trackers()
+
+    def __init__(self):
+        self.init_trackers()
+
+    def get_history(self):
+        return {
+            "train_loss": self.loss_critic_history, #Critic loss, named train due to bug
+            "MSE_loss": self.loss_mse_history,  
+        }
+
+    def init_trackers(self):
+        self.loss_critic_history = list()
+        self.loss_mse_history = list()
+        self.reset_epoch_trackers()
+
+
+
+    def update_train_loss(self, n_batches):
+        self.loss_critic_history.append(self.sum_loss_d / n_batches)
+        self.loss_mse_history.append(self.sum_loss_g / n_batches)
+
+    def add_to_train_loss(self, loss_d, loss_g):
+        self.sum_loss_d += loss_d.item()
+        self.sum_loss_g += loss_g.item()
+
+    def reset_epoch_trackers(self):
+        self.sum_loss_d = 0
+        self.sum_loss_g = 0
+
+    def update_test_loss(self, n_batches):
+        return
+    
+    def get_last_epoch(self):
+        return {key: value[-1] for key, value in self.get_history().items()}
+
+
+# Copied from https://github.com/jtcramer/wavegan/blob/master/wavegan.py#L8
+class PhaseShuffle(nn.Module):
+    """
+    Performs phase shuffling, i.e. shifting feature axis of a 3D tensor
+    by a random integer in {-n, n} and performing reflection padding where
+    necessary.
+    """
+
+    def __init__(self, shift_factor):
+        super(PhaseShuffle, self).__init__()
+        self.shift_factor = shift_factor
+
+    def forward(self, x):
+        if self.shift_factor == 0:
+            return x
+        # uniform in (L, R)
+        k_list = torch.Tensor(x.shape[0]).random_(0, 2 * self.shift_factor + 1) - self.shift_factor
+        k_list = k_list.numpy().astype(int)
+
+        # Combine sample indices into lists so that less shuffle operations
+        # need to be performed
+        k_map = {}
+        for idx, k in enumerate(k_list):
+            k = int(k)
+            if k not in k_map:
+                k_map[k] = []
+            k_map[k].append(idx)
+
+        # Make a copy of x for our output
+        x_shuffle = x.clone()
+
+        # Apply shuffle to each sample
+        for k, idxs in k_map.items():
+            if k > 0:
+                x_shuffle[idxs] = F.pad(x[idxs][..., :-k], (k, 0), mode='reflect')
+            else:
+                x_shuffle[idxs] = F.pad(x[idxs][..., -k:], (0, -k), mode='reflect')
+
+        assert x_shuffle.shape == x.shape, "{}, {}".format(x_shuffle.shape,
+                                                       x.shape)
+        return x_shuffle
+
+
+class PhaseRemove(nn.Module):
+    def __init__(self):
+        super(PhaseRemove, self).__init__()
+
+    def forward(self, x):
+        pass
+
+
+
+
+# Helper functions
+class Reshape(nn.Module):
+    def __init__(self, shape):
+        super(Reshape, self).__init__()
+        self.shape = shape
+        
+
+    def forward(self, x):
+        return torch.reshape(x, shape=self.shape)
+    
+
+class GetLSTMOutput(nn.Module):
+    def forward(self, x):
+        out, _ = x
+        return out
+
+        
+class print_output(nn.Module):
+    def __init__(self, text, inc_value, print):
+        super(print_output, self).__init__()
+        self.text = text
+        self.inc_value = inc_value
+        self.print = print  #True or False
+
+    def forward(self, x): 
+        if self.print:
+          print(f'{self.text}: {x.shape}')
+
+        return x
+    
+class print_seperator(nn.Module):
+    def __init__(self, text, inc_value, print):
+        super(print_seperator, self).__init__()
+        self.text = text
+        self.inc_value = inc_value
+        self.print = print #True or False
+
+    def forward(self, x):
+        if self.print:
+          print(f'--------------{self.text}---------------')
+        return x    
+    
+
+class Swap(nn.Module):
+    def __init__(self, *dims):
+        super(Swap, self).__init__()
+        self.dims = dims
+
+    def forward(self, x):
+        return x.permute(self.dims)
+
+
+
+
+
---
+++ WaveGAN_utils.py
@@ -0,0 +1,95 @@
+import torch
+import torch.nn as nn
+
+# from https://github.com/aladdinpersson/Machine-Learning-Collection/blob/master/ML/Pytorch/GANs/4.%20WGAN-GP/utils.py
+
+def gradient_penalty(critic, real, fake, device="cuda"):
+    BATCH_SIZE, C, L = real.shape
+    alpha = torch.rand((BATCH_SIZE, 1, 1)).repeat(1, C, L).to(device)
+    interpolated_samples = real * alpha + fake * (1 - alpha)
+
+    # Calculate critic scores
+    mixed_scores = critic(interpolated_samples)
+
+    # Take the gradient of the scores with respect to the samples
+    gradient = torch.autograd.grad(
+        inputs=interpolated_samples,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    gradient = gradient.reshape(gradient.shape[0], -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
+    return gradient_penalty
+
+
+def init_weights(self):
+        for module in self.modules():  
+            classname = module.__class__.__name__
+            if isinstance(module, (nn.Conv1d, nn.Conv2d)):
+                print(classname)
+                nn.init.orthogonal_(module.weight.data)
+                #nn.init.normal_(module.weight.data, 0, 0.02)
+                if module.bias is not None:
+                    nn.init.zeros_(module.bias)
+
+            elif isinstance(module, (nn.ConvTranspose1d, nn.ConvTranspose2d)):
+                print(classname)
+                nn.init.orthogonal_(module.weight.data)
+                #nn.init.normal_(module.weight.data, 0, 0.02)
+                if module.bias is not None:
+                    nn.init.zeros_(module.bias)
+              
+            elif isinstance(module, nn.LSTM):
+                 for name, param in module.named_parameters():
+                    if "weight_ih" in name:
+                        print(module)
+                        nn.init.xavier_uniform_(param.data)
+                    elif "weight_hh" in name:
+                        nn.init.orthogonal_(param.data)
+                    elif "bias" in name:
+                        param.data.fill_(0)
+                        # Initialize forget gate bias to 1 (common practice)
+                        n = param.size(0)
+                        param.data[n // 4 : n // 2].fill_(1)
+            elif isinstance(module, nn.Linear):
+                nn.init.xavier_uniform_(module.weight)
+                if module.bias is not None:
+                    nn.init.zeros_(module.bias)  
+
+            elif isinstance(module, nn.BatchNorm1d):
+                nn.init.normal_(module.weight.data, 0, 0.02)
+                nn.init.constant_(module.bias.data, 0)         
+
+
+
+# costum module getter to assert different learn rates
+def get_modules(self):
+         # Define different parameter groups
+         # Separate parameter lists
+            gen_params = []
+            critic_params = []
+
+            
+
+            for module in self.critic.modules():  # Only iterate over Critic modules
+                if isinstance(module, (nn.Conv1d, nn.Conv2d)):
+                    critic_params.append({"params": module.parameters(), "lr": 1e-4}) 
+                elif isinstance(module, nn.Linear):
+                    critic_params.append({"params": module.parameters(), "lr": 1e-4})   
+                elif isinstance(module, nn.BatchNorm1d):
+                    critic_params.append({"params": module.parameters(), "lr": 1e-4})        
+            
+            for module in self.gen.modules():  # Only iterate over Generator modules
+                if isinstance(module, (nn.ConvTranspose1d, nn.ConvTranspose2d)):
+                    gen_params.append({"params": module.parameters(), "lr": 1e-4}) 
+                elif isinstance(module, nn.LSTM):
+                    gen_params.append({"params": module.parameters(), "lr": 1e-4}) 
+                elif isinstance(module, nn.Linear):
+                    gen_params.append({"params": module.parameters(), "lr": 1e-4}) 
+                elif isinstance(module, nn.BatchNorm1d):
+                    gen_params.append({"params": module.parameters(), "lr": 1e-4}) 
+                     
+            return gen_params, critic_params
